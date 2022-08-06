64eb46e80af774d0d1304df02aa8d6ef7f8d5c4b
yun0529 <0529yun@naver.com>
2022-08-06 17:57:58 +0900

    initial commit

---
+++ app/src/main/java/com/example/template/config/KakaoAccessInterceptor.kt
@@ -0,0 +1,17 @@
+package com.example.template.config
+
+import com.example.template.config.ApplicationClass.Companion.ADDRESS_KEY
+import okhttp3.Interceptor
+import okhttp3.Request
+import okhttp3.Response
+import java.io.IOException
+import kotlin.jvm.Throws
+
+class KakaoAccessInterceptor : Interceptor {
+    @Throws(IOException::class)
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val builder: Request.Builder = chain.request().newBuilder()
+        builder.addHeader("Authorization","KakaoAK " + ADDRESS_KEY)
+        return chain.proceed(builder.build())
+    }
+}
